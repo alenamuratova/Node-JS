7a9f38092bbecd243541e0e4ea81ef8e5f75d661
Alenka777555 <alenka7775552@yandex.ru>
2019-03-02 17:22:00 +0300

    first commit

---
+++ HW_1/index.js
@@ -0,0 +1,5 @@
+var moment = require('moment');
+console.log(moment().format('dddd, MMMM DD YYYY, h:mm:ss'));
+
+const second = require('./second_file');
+const beep = require('./beep');
---
+++ HW_1/second_file.js
@@ -0,0 +1,6 @@
+const os = require('os');
+
+let userName = os.userInfo().username;
+ 
+console.log(userName);
+
